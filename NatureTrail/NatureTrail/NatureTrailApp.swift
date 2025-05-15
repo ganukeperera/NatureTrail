12b2fb5d5dae802904fe891d192b2bf203179726
ganuke <ganuke@gmail.com>
2025-05-15 06:48:51 +0530

    Add Xcode Project

---
+++ NatureTrail/NatureTrail/NatureTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureTrailApp.swift
+//  NatureTrail
+//
+//  Created by Ganuke Perera on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct NatureTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
