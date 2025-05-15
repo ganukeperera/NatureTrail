e06697a4788dfb1abadb7715af34edb90e18bd93
ganuke <ganuke@gmail.com>
2025-05-15 07:34:16 +0530

    Load data from jsonfile

---
+++ NatureTrail/NatureTrail/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  NatureTrail
+//
+//  Created by Ganuke Perera on 2025-05-15.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        let longitude: Double
+        let latitude: Double
+    }
+}
---
+++ NatureTrail/NatureTrail/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  NatureTrail
+//
+//  Created by Ganuke Perera on 2025-05-15.
+//
+
+import Foundation
+
+let landmarks: [Landmark] = load(fileName: "landmarkData.json")
+
+func load<T: Decodable>(fileName: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else {
+        fatalError("Couldn't find the \(fileName) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load data from \(file). Error: \(error)")
+    }
+    
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self). Error: \(error)")
+    }
+    
+}
